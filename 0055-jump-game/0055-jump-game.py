8b2edffe845c2a5b7a11371368a04c424170bac7
Heet Jatin Chheda <51861740+heetc27@users.noreply.github.com>
2023-10-03 00:55:41 -0400

    Time: 384 ms (22.36%), Space: 14.2 MB (75.13%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if not nums:
+            return False
+        
+        max_reach = 0
+        for i in range(len(nums)):
+            if i > max_reach:
+                return False
+            
+            max_reach = max(max_reach, i + nums[i])
+            
+            if max_reach >= len(nums) - 1:
+                return True
+        
+        return False
