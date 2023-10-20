d64685cca7f6bb7321c8670c9cb1b80a21bf0e6e
Heet Jatin Chheda <51861740+heetc27@users.noreply.github.com>
2023-10-20 18:53:49 -0400

    Time: 22 ms (52.11%), Space: 13.6 MB (13.61%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        citations.sort(reverse = True)
+        for i in range(len(citations)):
+            if (citations[i]>i):
+                pass
+            else:
+                return i
+        return len(citations)
